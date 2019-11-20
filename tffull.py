9cd45c6d288b3a0ad15ed98f8d8ef91b8abb8c84
zxsq-cc <44452124+zxsq-cc@users.noreply.github.com>
2019-11-20 10:01:38 -0500

    valueerror fix

--- tffull.py
+++ tffull.py
@@ -8,7 +8,7 @@ np.set_printoptions(suppress=True)
 model = tensorflow.keras.models.load_model('/home/pi/Desktop/tffull/keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 image = Image.open('/home/pi/Desktop/tffull/testimg.jpg')
-image.resize((224, 224))
+image = image.resize((224, 224))
 image_array = np.asarray(image)
 normalized_image_array = image_array / 255.0
 data[0] = normalized_image_array
